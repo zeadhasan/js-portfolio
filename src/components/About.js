2bc52760488d6eeb8764071562bec3bcbe5df326
zeadhasan <zeadhasan1995shuvo@gmail.com>
2022-08-16 00:03:17 +0600

    sm about part problems solve commit

--- src/components/About.js
+++ src/components/About.js
@@ -7,12 +7,12 @@ const About = () => {
       className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-        <div className="pb-8 sm:mt-5 ">
+        <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             About
           </p>
         </div>
-        <p className="text-xl mt-20">
+        <p className="text-xl mt-12">
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius,
           deserunt illum mollitia officiis qui exercitationem perferendis neque
           quasi a recusandae necessitatibus tempora iusto! Blanditiis error
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 
 const Home = () => {
   return (
-    <div name="home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
+    <div name="home" className='pt-10 h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-4xl sm:text-7xl font-bold text-white">I'm a Full Stack Developer </h2>
@@ -23,7 +23,7 @@ const Home = () => {
           </div>
         </div>
         <div>
-            <img className="rounded-2xl mx-auto w-2/3 md:w-full" src={HeroImage} alt="HeroImage" />
+            <img className="rounded-2xl mx-auto w-2/3 md:w-full mb-44" src={HeroImage} alt="HeroImage" />
         </div>
       </div>
     </div>
